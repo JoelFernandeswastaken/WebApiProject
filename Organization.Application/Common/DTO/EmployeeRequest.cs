236d63eacee3755ce456e22fb7820d80b7cbdfa1
Joel Fernandes <joel18052001@gmail.com>
2024-05-31 16:52:56 +0530

    Added employee controller endpoints

---
+++ Organization.Application/Common/DTO/EmployeeRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organization.Application.Common.DTO
+{
+    public class EmployeeRequest
+    {
+        public int age {  get; set; }   
+        public string name { get; set; }    
+        public string position { get; set; }
+        public string companyID { get; set; }   
+        public int salary { get; set; }
+    }
+}
---
+++ Organization.WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Validations;
+using Organization.Application.Common.DTO;
+using Organization.Application.Common.Interfaces.Persistance;
+using Organization.Domain.Employee.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Organization.Presentation.Api.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public EmployeesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("GetEmployees")]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var result = await _unitOfWork.Employees.GetAsyncOld();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("Employee/{id}")]
+        public async Task<IActionResult> GetEmployeeByID(string id)
+        {
+            var employee = await _unitOfWork.Employees.GetByIdAsync(id);
+            if(employee == null)
+                return NotFound();
+            else 
+                return Ok(employee);
+        }
+
+        [HttpPost]
+        [Route("AddEmployee")]
+        public async Task<IActionResult> AddEmployee(EmployeeRequest employee)
+        {
+            string guid = Guid.NewGuid().ToString().Replace("/", "_").Replace("+", "-").Substring(0, 22);
+            _unitOfWork.BeginTransaction();
+            var id = _unitOfWork.Employees.AddAsync(new Employee()
+            {
+                Id = guid,
+                Name = employee.name,
+                Position = employee.position,   
+                CompanyId = employee.companyID,
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now,  
+                Salary = employee.salary,
+                Age = employee.age
+            });
+            _unitOfWork.CommitAndCloseConnection();
+
+            return CreatedAtAction("GetEmployeeByID", new { id = id }, employee);            
+        }
+
+        [HttpPut]
+        [Route("UpdateEmployee")]
+        public async Task<IActionResult> UpdateEmployee(string id, EmployeeRequest employeeRequest)
+        {
+            var requiredEmployee = await _unitOfWork.Employees.GetByIdAsync(id);
+            if(requiredEmployee == null) 
+                return NotFound(employeeRequest);
+            else
+            {
+                if(requiredEmployee.Id != id)
+                     return BadRequest(requiredEmployee.Id);
+
+                requiredEmployee.Name = employeeRequest.name;
+                requiredEmployee.Age = employeeRequest.age;
+                requiredEmployee.CompanyId = employeeRequest.companyID; 
+                requiredEmployee.Salary = employeeRequest.salary;
+                requiredEmployee.Position = employeeRequest.position;
+                requiredEmployee.ModifiedOn = DateTime.Now;
+
+                _unitOfWork.BeginTransaction();
+                var result = await _unitOfWork.Employees.UpdateAsync(requiredEmployee);
+                _unitOfWork.CommitAndCloseConnection();
+
+                return result ? Ok("Record Updated successfully") : BadRequest("Something went wrong");
+            }
+        }
+        [HttpDelete]
+        [Route("DelelteEmployee")]
+        public async Task<IActionResult> DeleteEmployee(string id)
+        {
+            var employeeToDelete = _unitOfWork.Employees.GetByIdAsync(id);
+            if (employeeToDelete == null)
+                return NotFound(employeeToDelete);
+            _unitOfWork.BeginTransaction();
+            await _unitOfWork.Employees.SoftDeleteAsync(id);
+            _unitOfWork.CommitAndCloseConnection();
+            return NoContent();
+        }
+    }
+}
