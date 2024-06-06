de521999b159d4732954e9e719322c1e143a4dee
Joel Fernandes <joel18052001@gmail.com>
2024-06-06 23:14:48 +0530

    Add query parameters for comany and employee

---
+++ Organization.Domain/Company/CompanyQueryParameters.cs
@@ -0,0 +1,13 @@
+﻿using Organization.Domain.Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organization.Domain.Company
+{
+    public sealed class CompanyQueryParameters : QueryParameters
+    {
+    }
+}
---
+++ Organization.Domain/Employee/EmployeeQueryParameters.cs
@@ -0,0 +1,13 @@
+﻿using Organization.Domain.Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organization.Domain.Employee
+{
+    public sealed class EmployeeQueryParameters : QueryParameters
+    {
+    }
+}
