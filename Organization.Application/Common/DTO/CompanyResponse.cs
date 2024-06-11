71df6b68f15ed49c706f71e1b03cea1d5c884970
Joel Fernandes <joel18052001@gmail.com>
2024-06-11 13:30:00 +0530

    Added response classes for employee and company

---
+++ Organization.Application/Common/DTO/CompanyResponse.cs
@@ -0,0 +1,17 @@
+﻿using Organization.Domain.Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organization.Application.Common.DTO
+{
+    public class CompanyResponse
+    {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+        public string? Address { get; set; }
+    }
+}
---
+++ Organization.Application/Common/DTO/EmployeeResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organization.Application.Common.DTO
+{
+    public class EmployeeResponse
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Position { get; set; } =string.Empty;
+        public int Salary { get; set; }
+        public string CreatedOn { get; set; }
+    }
+}
