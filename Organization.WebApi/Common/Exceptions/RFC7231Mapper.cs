16ef62883a39179ad149d23c16c1535cf6d04c56
Joel Fernandes <joel18052001@gmail.com>
2024-08-24 14:07:39 +0530

    Indented code in RFC mapper

--- Organization.WebApi/Common/Exceptions/RFC7231Mapper.cs
+++ Organization.WebApi/Common/Exceptions/RFC7231Mapper.cs
@@ -3,15 +3,15 @@
     public static class RFC7231Mapper
     {
         private static readonly Dictionary<int, string> StatusCodeToSectionMap = new Dictionary<int, string>
-    {
-        { 400, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1" },
-        { 401, "https://datatracker.ietf.org/doc/html/rfc7235#section-3.1" },
-        { 403, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.3" },
-        { 404, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4" },
-        { 405, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.5" },
-        { 406, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.6" },
-        { 500, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1" },
-    };
+        {
+            { 400, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1" },
+            { 401, "https://datatracker.ietf.org/doc/html/rfc7235#section-3.1" },
+            { 403, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.3" },
+            { 404, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4" },
+            { 405, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.5" },
+            { 406, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.6" },
+            { 500, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1" },
+        };
 
         public static string GetRFC7231Link(int? statusCode)
         {
